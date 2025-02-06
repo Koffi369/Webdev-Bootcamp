095325330dde4c2fab95a14b685494b69a615f6b
koffi <koffivigbagbe@gmail.com>
2025-02-05 23:41:47 -0500

    Adding folders

---
+++ JavaScript_Mastery_Complete_Course_yt/course.js
@@ -0,0 +1,41 @@
+var age = 25
+var colors = new Array("red", "green") 
+var other_color= ["blue", "yellow", "gray", "white"]
+
+console.log(other_color.length)
+
+
+var joke="The chicken crossed the road"
+
+console.log(joke[4])
+console.log(joke.length)
+console.log(other_color)
+
+
+// 1h 02 min: The splice method /  push  / sort
+console.log(" ")
+console.log("The splice method")
+console.log(" ")
+console.log(other_color.splice(1,2))
+console.log(other_color)
+ 
+console.log(" ")
+console.log("The push method")
+console.log(" ")
+console.log(other_color.push("black"))
+console.log(other_color)
+
+
+console.log(" ")
+console.log("The sort method")
+console.log(" ") 
+console.log(other_color.sort())
+console.log(other_color) 
+
+
+
+
+
+
+
+
