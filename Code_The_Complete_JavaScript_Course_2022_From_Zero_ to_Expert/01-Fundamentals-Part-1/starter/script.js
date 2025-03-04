076b3d0ff4c985122e077dd642a77771f13f9712
koffi <koffivigbagbe@gmail.com>
2025-03-04 14:33:38 -0500

    Finished Fondamentals part1

--- Code_The_Complete_JavaScript_Course_2022_From_Zero_ to_Expert/01-Fundamentals-Part-1/starter/script.js
+++ Code_The_Complete_JavaScript_Course_2022_From_Zero_ to_Expert/01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,43 @@
 let js = 'Amazing'
-if (js === 'Amazing' ) alert('JS is fun')
+if (js === 'Amazing' ) alert('JS is fun');
+console.log("May God help me")
+
+
+console.log('string with \n\
+    multiple lines 😁  ');
+
+
+console.log('1'- 1)
+
+
+console.log('1'+  1);
+
+let m = '1'+  1;
+
+console.log(typeof m);
+
+
+const userID = Number(prompt("What's your ID"))
+
+console.log(userID)
+
+
+// Condidtional (ternary ) operator
+
+const clientAge = 96
+
+clientAge >= 18? console.log('Client can drink wine 🍷'):
+        console.log('Client can only  drink water 🚰')
+
+
+const drink = clientAge >= 21 ? 'wine' : 'water'
+console.log(drink)
+
+
+const client2Age = 18
+
+console.log(client2Age >= 21 ? 'wine' : 'water')
+
+
+
+
