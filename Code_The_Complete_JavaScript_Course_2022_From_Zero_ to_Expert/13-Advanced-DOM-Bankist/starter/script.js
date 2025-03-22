ff6293fc0c2e8b13513f53fa1a2ac6a34360f1cb
koffi <koffivigbagbe@gmail.com>
2025-03-22 21:00:57 +0000

    event handling and removing

--- Code_The_Complete_JavaScript_Course_2022_From_Zero_ to_Expert/13-Advanced-DOM-Bankist/starter/script.js
+++ Code_The_Complete_JavaScript_Course_2022_From_Zero_ to_Expert/13-Advanced-DOM-Bankist/starter/script.js
@@ -50,12 +50,50 @@ Message.innerHTML =
 
 const headerel = document.querySelector('.header');
 
-// headerel.prepend(Message);
+headerel.prepend(Message);
 
 // // headerel.append(Message);
-// headerel.append(Message.cloneNode(true));
+headerel.append(Message.cloneNode(true));
 
-headerel.before(Message);
-headerel.after(Message);
+// headerel.before(Message);
+// headerel.after(Message);
 
 ////////////// Video 006 Styles, Attributes and Classes
+
+Message.style.backgroundColor = 'green';
+
+console.log(getComputedStyle(Message));
+
+console.log(getComputedStyle(Message).color);
+// headerel.setAttribute('attributeName')
+// headerel.getAttribute('attributeName')
+
+console.log(Message.classList);
+Message.classList.add('newclass');
+Message.classList.remove('newclass');
+console.log(Message.classList);
+
+Message.classList.toggle('newclass');
+console.log(Message.classList.contains('newclass'));
+console.log(Message.classList);
+
+////////////// Video 007 Implementing Smooth scroll
+
+const scrollBtn = document.querySelector('.btn--scroll-to');
+const scrollToSection = document.querySelector('#section--1');
+
+scrollBtn.addEventListener('click', function (e) {
+  scrollToSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+////////////// Video 008 Types of Events and Events Handlers
+
+const h1Elm = document.querySelector('h1');
+
+const alertFunc = function (e) {
+  alert('Video 008 Types of Events and Events Handlers ');
+
+  h1Elm.removeEventListener('mouseenter', alertFunc);
+};
+
+h1Elm.addEventListener('mouseenter', alertFunc);
