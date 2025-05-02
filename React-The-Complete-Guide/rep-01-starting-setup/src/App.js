0fc774ebacb9f81b4d41b1747ffa97a637b3bbc2
koffi <koffivigbagbe@gmail.com>
2025-05-02 14:40:35 +0000

    started the replication from video 9

--- React-The-Complete-Guide/rep-01-starting-setup/src/App.js
+++ React-The-Complete-Guide/rep-01-starting-setup/src/App.js
@@ -1,9 +1,15 @@
+import MyExpenseItem from "./Components/MyExpenseItem";
+
 function App() {
   return (
     <div>
+      <MyExpenseItem></MyExpenseItem>
       <h2>Let's get started!</h2>
     </div>
   );
 }
+
+// export NODE_OPTIONS=--openssl-legacy-provider
+// PORT=3001 npm start
 
 export default App;
---
+++ React-The-Complete-Guide/rep-01-starting-setup/src/Components/MyExpenseItem.js
@@ -0,0 +1,11 @@
+import "./MyExpenseItem.css";
+
+function MyExpenseItem() {
+  return (
+    <div>
+      <p>MyExpenseItem tag</p>
+    </div>
+  );
+}
+
+export default MyExpenseItem;
