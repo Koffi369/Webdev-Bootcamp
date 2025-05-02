1f24dbdce6289927b9dbd879b938d83c154ea85a
koffi <koffivigbagbe@gmail.com>
2025-05-02 14:56:55 +0000

    video 11

--- React-The-Complete-Guide/rep-01-starting-setup/src/Components/MyExpenseItem.js
+++ React-The-Complete-Guide/rep-01-starting-setup/src/Components/MyExpenseItem.js
@@ -1,9 +1,33 @@
+///////////////// Video 11
+
+// import "./MyExpenseItem.css";
+
+// function MyExpenseItem() {
+//   return (
+//     <div className="expense-item">
+//       <div>March 28th 2021</div>
+//       <div className="expense-item__description">
+//         <h2>Car Insurance</h2>
+//         <div className="expense-item__price">$ 294.67</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MyExpenseItem;
+
+///////////////// Video 12
+
 import "./MyExpenseItem.css";
 
 function MyExpenseItem() {
   return (
-    <div>
-      <p>MyExpenseItem tag</p>
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$ 294.67</div>
+      </div>
     </div>
   );
 }
