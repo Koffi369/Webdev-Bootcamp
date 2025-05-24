3ed58c0c33f52b36d7b0162ae744b5df655b18ed
koffi <koffivigbagbe@gmail.com>
2025-05-24 18:15:05 +0000

    FInished chap 9

--- React-The-Complete-Guide/chap08-01-starting-project/src/components/UI/ErrorModal.js
+++ React-The-Complete-Guide/chap08-01-starting-project/src/components/UI/ErrorModal.js
@@ -1,25 +1,223 @@
+// import React from "react";
+// import Card from "./Card";
+// import styles from "./ErrorModal.module.css";
+// import Button from "./Button";
+
+// function ErrorModal(props) {
+//   return (
+//     <div>
+//       <div className={styles.backdrop} onClick={props.onclickOkay}></div>
+//       <Card className={styles.modal}>
+//         <header className={styles.header}>
+//           <h2>{props.errorTitle}</h2>
+//         </header>
+
+//         <div className={styles.content}>
+//           <p>{props.errorMessage} </p>
+//         </div>
+//         <footer className={styles.actions}>
+//           <Button onClick={props.onclickOkay}>Okay</Button>
+//         </footer>
+//       </Card>
+//     </div>
+//   );
+// }
+
+// export default ErrorModal;
+
+//
+
+//
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////                Chap 09_Diving_Deeper_Working_with_Fragments,_Portals_&_Refs
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+//
+
+//
+
+//
+
+//
+
+////////////////////////////////////////////////////////////////////////
+
+////////////////// Video 003 Creating a wrapper component
+
+//
+
+//
+
+// import React from "react";
+// import Card from "./Card";
+// import styles from "./ErrorModal.module.css";
+// import Button from "./Button";
+// import Wrapper from "../Helpers/Wrapper";
+
+// function ErrorModal(props) {
+//   return (
+//     <Wrapper>
+//       <div className={styles.backdrop} onClick={props.onclickOkay}></div>
+//       <Card className={styles.modal}>
+//         <header className={styles.header}>
+//           <h2>{props.errorTitle}</h2>
+//         </header>
+
+//         <div className={styles.content}>
+//           <p>{props.errorMessage} </p>
+//         </div>
+//         <footer className={styles.actions}>
+//           <Button onClick={props.onclickOkay}>Okay</Button>
+//         </footer>
+//       </Card>
+//     </Wrapper>
+//   );
+// }
+
+// export default ErrorModal;
+
+//
+
+//
+
+////////////////////////////////////////////////////////////////////////
+
+////////////////// Video 004 React fragment
+
+//
+
+//
+
+// import React from "react";
+// import Card from "./Card";
+// import styles from "./ErrorModal.module.css";
+// import Button from "./Button";
+// // import Wrapper from "../Helpers/Wrapper";
+
+// function ErrorModal(props) {
+//   return (
+//     // <>
+//     //   <div className={styles.backdrop} onClick={props.onclickOkay}></div>
+//     //   <Card className={styles.modal}>
+//     //     <header className={styles.header}>
+//     //       <h2>{props.errorTitle}</h2>
+//     //     </header>
+
+//     //     <div className={styles.content}>
+//     //       <p>{props.errorMessage} </p>
+//     //     </div>
+//     //     <footer className={styles.actions}>
+//     //       <Button onClick={props.onclickOkay}>Okay</Button>
+//     //     </footer>
+//     //   </Card>
+//     // </>
+
+//     <React.Fragment>
+//       <div className={styles.backdrop} onClick={props.onclickOkay}></div>
+//       <Card className={styles.modal}>
+//         <header className={styles.header}>
+//           <h2>{props.errorTitle}</h2>
+//         </header>
+
+//         <div className={styles.content}>
+//           <p>{props.errorMessage} </p>
+//         </div>
+//         <footer className={styles.actions}>
+//           <Button onClick={props.onclickOkay}>Okay</Button>
+//         </footer>
+//       </Card>
+//     </React.Fragment>
+//   );
+// }
+
+// export default ErrorModal;
+
+//
+
+//
+
+////////////////////////////////////////////////////////////////////////
+
+////////////////// Video 006 Working with Portal
+
+//
+
+//
+
+// import React from "react";
+// import Card from "./Card";
+// import styles from "./ErrorModal.module.css";
+// import Button from "./Button";
+
+// function ErrorModal(props) {
+//   return (
+//     <React.Fragment>
+//       <div className={styles.backdrop} onClick={props.onclickOkay}></div>
+//       <Card className={styles.modal}>
+//         <header className={styles.header}>
+//           <h2>{props.errorTitle}</h2>
+//         </header>
+
+//         <div className={styles.content}>
+//           <p>{props.errorMessage} </p>
+//         </div>
+//         <footer className={styles.actions}>
+//           <Button onClick={props.onclickOkay}>Okay</Button>
+//         </footer>
+//       </Card>
+//     </React.Fragment>
+//   );
+// }
+
+// export default ErrorModal;
+
 import React from "react";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
 import Button from "./Button";
 
+import ReactDom from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onclickOkay}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.errorTitle}</h2>
+      </header>
+
+      <div className={styles.content}>
+        <p>{props.errorMessage} </p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onclickOkay}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 function ErrorModal(props) {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onclickOkay}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.errorTitle}</h2>
-        </header>
-
-        <div className={styles.content}>
-          <p>{props.errorMessage} </p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onclickOkay}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onclickOkay={props.onclickOkay} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          errorTitle={props.errorTitle}
+          errorMessage={props.errorMessage}
+          onclickOkay={props.onclickOkay}
+        ></ModalOverlay>,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 }
 
--- React-The-Complete-Guide/chap08-01-starting-project/src/components/Users/AddUser.js
+++ React-The-Complete-Guide/chap08-01-starting-project/src/components/Users/AddUser.js
@@ -1,19 +1,378 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// import Card from "../UI/Card";
+// import styles from "./AddUser.module.css";
+// import Button from "../UI/Button";
+// import ErrorModal from "../UI/ErrorModal";
+
+// function AddUser(props) {
+//   const [enteredUsername, setenteredUsername] = useState("");
+//   const [enteredAge, setenteredAge] = useState("");
+
+//   const [error, setError] = useState("");
+//   console.log("error:", error);
+
+//   function addUserHandler(event) {
+//     event.preventDefault();
+
+//     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+//       setError({
+//         errorTitle: "Invalid input",
+//         errorMessage: "Please enter a valid name and age",
+//       });
+//       return;
+//     }
+
+//     if (Number(enteredAge) < 1) {
+//       setError({
+//         errorTitle: "Invalid age",
+//         errorMessage: "Please enter a valid  age",
+//       });
+//       return;
+//     }
+
+//     // console.log("setenteredUsername :", enteredUsername);
+//     // console.log("setenteredAge :", enteredAge);
+
+//     props.onAddUser(enteredUsername, enteredAge);
+
+//     setenteredUsername("");
+//     setenteredAge("");
+//   }
+
+//   function usernameChangeHandler(event) {
+//     setenteredUsername(event.target.value);
+//   }
+
+//   function ageChangeHandler(event) {
+//     setenteredAge(event.target.value);
+//   }
+
+//   const errorHandler = () => {
+//     setError(null);
+//   };
+
+//   return (
+//     <div>
+//       {/* <ErrorModa errorTitle= {}  errorMessage = {} actions = {}></ErrorModal> */}
+//       {error && (
+//         <ErrorModal
+//           errorTitle={error.errorTitle}
+//           errorMessage={error.errorMessage}
+//           onclickOkay={errorHandler}
+//         ></ErrorModal>
+//       )}
+
+//       <Card className={styles.input}>
+//         <form onSubmit={addUserHandler}>
+//           <label htmlFor="Username">Username</label>
+//           <input
+//             id="username"
+//             type="text"
+//             onChange={usernameChangeHandler}
+//             value={enteredUsername}
+//           ></input>
+//           <label htmlFor="age">Age (Years)</label>
+//           <input
+//             id="age"
+//             type="text"
+//             onChange={ageChangeHandler}
+//             value={enteredAge}
+//           ></input>
+//           <Button type="submit">Add User</Button>
+//         </form>
+//       </Card>
+//     </div>
+//   );
+// }
+
+// export default AddUser;
+
+//
+
+//
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////                Chap 09_Diving_Deeper_Working_with_Fragments,_Portals_&_Refs
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+//
+
+//
+
+//
+
+//
+
+////////////////////////////////////////////////////////////////////////
+
+////////////////// Video 003 Creating a wrapper component
+
+//
+
+//
+
+// import React, { useState } from "react";
+
+// import Card from "../UI/Card";
+// import styles from "./AddUser.module.css";
+// import Button from "../UI/Button";
+// import ErrorModal from "../UI/ErrorModal";
+// import Wrapper from "../Helpers/Wrapper";
+
+// function AddUser(props) {
+//   const [enteredUsername, setenteredUsername] = useState("");
+//   const [enteredAge, setenteredAge] = useState("");
+
+//   const [error, setError] = useState("");
+//   console.log("error:", error);
+
+//   function addUserHandler(event) {
+//     event.preventDefault();
+
+//     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+//       setError({
+//         errorTitle: "Invalid input",
+//         errorMessage: "Please enter a valid name and age",
+//       });
+//       return;
+//     }
+
+//     if (Number(enteredAge) < 1) {
+//       setError({
+//         errorTitle: "Invalid age",
+//         errorMessage: "Please enter a valid  age",
+//       });
+//       return;
+//     }
+
+//     // console.log("setenteredUsername :", enteredUsername);
+//     // console.log("setenteredAge :", enteredAge);
+
+//     props.onAddUser(enteredUsername, enteredAge);
+
+//     setenteredUsername("");
+//     setenteredAge("");
+//   }
+
+//   function usernameChangeHandler(event) {
+//     setenteredUsername(event.target.value);
+//   }
+
+//   function ageChangeHandler(event) {
+//     setenteredAge(event.target.value);
+//   }
+
+//   const errorHandler = () => {
+//     setError(null);
+//   };
+
+//   return (
+//     <Wrapper>
+//       {/* <ErrorModa errorTitle= {}  errorMessage = {} actions = {}></ErrorModal> */}
+//       {error && (
+//         <ErrorModal
+//           errorTitle={error.errorTitle}
+//           errorMessage={error.errorMessage}
+//           onclickOkay={errorHandler}
+//         ></ErrorModal>
+//       )}
+
+//       <Card className={styles.input}>
+//         <form onSubmit={addUserHandler}>
+//           <label htmlFor="Username">Username</label>
+//           <input
+//             id="username"
+//             type="text"
+//             onChange={usernameChangeHandler}
+//             value={enteredUsername}
+//           ></input>
+//           <label htmlFor="age">Age (Years)</label>
+//           <input
+//             id="age"
+//             type="text"
+//             onChange={ageChangeHandler}
+//             value={enteredAge}
+//           ></input>
+//           <Button type="submit">Add User</Button>
+//         </form>
+//       </Card>
+//     </Wrapper>
+//   );
+// }
+
+// export default AddUser;
+
+//
+
+//
+
+////////////////////////////////////////////////////////////////////////
+
+////////////////// Video 004 React fragment
+
+//
+
+//
+
+// import React, { useState } from "react";
+
+// import Card from "../UI/Card";
+// import styles from "./AddUser.module.css";
+// import Button from "../UI/Button";
+// import ErrorModal from "../UI/ErrorModal";
+// // import Wrapper from "../Helpers/Wrapper";
+
+// function AddUser(props) {
+//   const [enteredUsername, setenteredUsername] = useState("");
+//   const [enteredAge, setenteredAge] = useState("");
+
+//   const [error, setError] = useState("");
+//   console.log("error:", error);
+
+//   function addUserHandler(event) {
+//     event.preventDefault();
+
+//     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+//       setError({
+//         errorTitle: "Invalid input",
+//         errorMessage: "Please enter a valid name and age",
+//       });
+//       return;
+//     }
+
+//     if (Number(enteredAge) < 1) {
+//       setError({
+//         errorTitle: "Invalid age",
+//         errorMessage: "Please enter a valid  age",
+//       });
+//       return;
+//     }
+
+//     // console.log("setenteredUsername :", enteredUsername);
+//     // console.log("setenteredAge :", enteredAge);
+
+//     props.onAddUser(enteredUsername, enteredAge);
+
+//     setenteredUsername("");
+//     setenteredAge("");
+//   }
+
+//   function usernameChangeHandler(event) {
+//     setenteredUsername(event.target.value);
+//   }
+
+//   function ageChangeHandler(event) {
+//     setenteredAge(event.target.value);
+//   }
+
+//   const errorHandler = () => {
+//     setError(null);
+//   };
+
+//   return (
+//     // <>
+//     //   {/* <ErrorModa errorTitle= {}  errorMessage = {} actions = {}></ErrorModal> */}
+//     //   {error && (
+//     //     <ErrorModal
+//     //       errorTitle={error.errorTitle}
+//     //       errorMessage={error.errorMessage}
+//     //       onclickOkay={errorHandler}
+//     //     ></ErrorModal>
+//     //   )}
+
+//     //   <Card className={styles.input}>
+//     //     <form onSubmit={addUserHandler}>
+//     //       <label htmlFor="Username">Username</label>
+//     //       <input
+//     //         id="username"
+//     //         type="text"
+//     //         onChange={usernameChangeHandler}
+//     //         value={enteredUsername}
+//     //       ></input>
+//     //       <label htmlFor="age">Age (Years)</label>
+//     //       <input
+//     //         id="age"
+//     //         type="text"
+//     //         onChange={ageChangeHandler}
+//     //         value={enteredAge}
+//     //       ></input>
+//     //       <Button type="submit">Add User</Button>
+//     //     </form>
+//     //   </Card>
+//     // </>
+
+//     <React.Fragment>
+//       {/* <ErrorModa errorTitle= {}  errorMessage = {} actions = {}></ErrorModal> */}
+//       {error && (
+//         <ErrorModal
+//           errorTitle={error.errorTitle}
+//           errorMessage={error.errorMessage}
+//           onclickOkay={errorHandler}
+//         ></ErrorModal>
+//       )}
+
+//       <Card className={styles.input}>
+//         <form onSubmit={addUserHandler}>
+//           <label htmlFor="Username">Username</label>
+//           <input
+//             id="username"
+//             type="text"
+//             onChange={usernameChangeHandler}
+//             value={enteredUsername}
+//           ></input>
+//           <label htmlFor="age">Age (Years)</label>
+//           <input
+//             id="age"
+//             type="text"
+//             onChange={ageChangeHandler}
+//             value={enteredAge}
+//           ></input>
+//           <Button type="submit">Add User</Button>
+//         </form>
+//       </Card>
+//     </React.Fragment>
+//   );
+// }
+
+// export default AddUser;
+
+//
+
+//
+
+////////////////////////////////////////////////////////////////////////
+
+////////////////// Video 007 Working with Refs
+
+//
+
+//
+
+import React, { useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+// import Wrapper from "../Helpers/Wrapper";
 
 function AddUser(props) {
   const [enteredUsername, setenteredUsername] = useState("");
   const [enteredAge, setenteredAge] = useState("");
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const [error, setError] = useState("");
   console.log("error:", error);
 
   function addUserHandler(event) {
     event.preventDefault();
+    console.log("nameInputRef: ", nameInputRef.current.value);
+    console.log("ageInputRef: ", ageInputRef.current.value);
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
@@ -53,7 +412,7 @@ function AddUser(props) {
   };
 
   return (
-    <div>
+    <React.Fragment>
       {/* <ErrorModa errorTitle= {}  errorMessage = {} actions = {}></ErrorModal> */}
       {error && (
         <ErrorModal
@@ -71,6 +430,7 @@ function AddUser(props) {
             type="text"
             onChange={usernameChangeHandler}
             value={enteredUsername}
+            ref={nameInputRef}
           ></input>
           <label htmlFor="age">Age (Years)</label>
           <input
@@ -78,11 +438,12 @@ function AddUser(props) {
             type="text"
             onChange={ageChangeHandler}
             value={enteredAge}
+            ref={ageInputRef}
           ></input>
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 }
 
